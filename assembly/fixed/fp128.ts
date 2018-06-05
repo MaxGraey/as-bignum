98a49ca3d575bc08d03af9fb3bc93db1163101f6
MaxGraey <maxgraey@gmail.com>
2018-06-05 14:38:31 +0300

    Some basic methods for fp128

--- assembly/fixed/fp128.ts
+++ assembly/fixed/fp128.ts
@@ -1,8 +1,35 @@
 import { i128 } from '../integer/i128';
+import { u128 } from '../integer/u128';
+
+// Support only this fractions:
+// fp128<u8>  =>  8 bits
+// fp128<u16> => 16 bits
+// fp128<u32> => 32 bits
 
 export class fp128<Q> {
 
-  constructor(public value: i128 = i128.Zero) {}
+  protected value: i128 = i128.Zero;
+
+  constructor(lo: i64 = 0, hi: i64 = 0) {
+    this.lo = lo;
+    this.hi = hi;
+  }
+
+  @inline
+  get fractBits(): i32 {
+    return 8 * sizeof<Q>();
+  }
+
+  /*
+  intPart(): i128 {
+    return this.value >> (8 * sizeof<Q>);
+  }
+  */
+
+  @inline
+  get fractPart(): u128 {
+    return u128.shl(changetype<u128>(this.value), 32 - 8 * sizeof<Q>);
+  }
 
   // TODO
 }
