f61375174055345ffbe5d8dd09eafec6768591b6
MaxGraey <maxgraey@gmail.com>
2018-07-21 12:48:48 +0300

    (chore) update configs and refactoring tests

--- tests/helloword.spec.ts
+++ tests/helloword.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { describe, it } from "mocha";
 import * as fs from 'fs';
-import { comparei32withU128 } from './utils/test_utils';
+import { comparei32withU128 } from './utils/helpers';
 
 describe('Sanity check for WebAssembly', () => {
   it("should be available", () => {
