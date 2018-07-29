7ab6932dccdb83cab1d016298a6124805e900121
MaxGraey <maxgraey@gmail.com>
2018-07-29 11:17:22 +0300

    try fix async issue again

--- tests/u128.spec.ts
+++ tests/u128.spec.ts
@@ -15,15 +15,20 @@ const memory = new WebAssembly.Memory({ initial: 2 });
 const buffer = new Uint8Array(memory.buffer);
 const imports = buildImports('u128.spec.as', memory, buffer);
 
-describe("u128", async () => {
-  const file   = await readFile(path.join(__dirname, 'build/u128.wasm'));
-  const result = await WebAssembly.instantiate(file, imports);
-  const instance: any = demangle(result.instance.exports);
+describe("u128", function () {
+  let instance: any;
+
+  before(async function (done) {
+    const file   = await readFile(path.join(__dirname, 'build/u128.wasm'));
+    const result = await WebAssembly.instantiate(file, imports);
+    instance = demangle(result.instance.exports);
+    done();
+  });
 
   for (const tests in instance) {
-    describe(camelToSpaced(tests), () => {
+    describe(camelToSpaced(tests), function () {
       for (const test in instance[tests]) {
-        it(camelToSpaced(test), () => {
+        it(camelToSpaced(test), function () {
           expect(instance[tests][test]()).to.be.eq(1);
         });
       }
