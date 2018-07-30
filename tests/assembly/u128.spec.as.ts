b39bc497caa2d47f6e74c524af05978b17098eab
MaxGraey <maxgraey@gmail.com>
2018-07-30 16:03:50 +0300

    Add more tests for u128. Minor refactorings

--- tests/assembly/u128.spec.as.ts
+++ tests/assembly/u128.spec.as.ts
@@ -5,19 +5,17 @@ declare function logString(len: i32, pointer: usize): void;
 
 export class StringConversionTest {
   static shouldConvertToDecimalString1(): boolean {
-    let a = new u128(10248516654965971928, 5);
-    let stringA = a.toString();
-    //logString(stringA.length, stringA.toUTF8());
-    return '102482237023513730008' == stringA;
+    var a = new u128(10248516654965971928, 5);
+    return '102482237023513730008' == a.toString();
   }
 
   static shouldConvertToDecimalString2(): boolean {
-    let a = new u128(0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF);
+    var a = new u128(0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF);
     return '340282366920938463463374607431768211455' == a.toString();
   }
 
   static shouldConvertToDecimalString3(): boolean {
-    let a = u128.Zero;
+    var a = u128.Zero;
     return '0' == a.toString();
   }
 }
@@ -25,116 +23,116 @@ export class StringConversionTest {
 export class BasicOperationsTest {
 
   static shouldEquateTwoNumbers(): boolean {
-    let a = new u128(100, 255);
-    let b = new u128(100, 255);
-    let eq = a == b;
-    return eq == true;
+    var a = new u128(100, 255);
+    var b = new u128(100, 255);
+    return a == b;
+  }
+
+  static shouldNonEquateTwoNumbers(): boolean {
+    var a = new u128(1, 1);
+    var b = new u128(1, 0);
+    return a != b;
   }
 
   static shouldCompareLessTwoNumbers1(): boolean {
-    let a = new u128(100, 100);
-    let b = new u128(50,  100);
-    let lt = b < a;
-    return lt == true;
+    var a = new u128(100, 100);
+    var b = new u128(50,  100);
+    return b < a;
   }
 
   static shouldCompareLessTwoNumbers2(): boolean {
-    let a = new u128(100, 100);
-    let b = new u128(100, 100);
-    let lt = b < a;
-    return lt == false;
+    var a = new u128(100, 100);
+    var b = new u128(100, 100);
+    return !(b < a);
   }
 
   static shouldCompareLessOrEualTwoNumbers1(): boolean {
-    let a = new u128(100, 100);
-    let b = new u128(50,  100);
-    let lt = b <= a;
-    return lt == true;
+    var a = new u128(100, 100);
+    var b = new u128(50,  100);
+    return b <= a;
   }
 
   static shouldCompareLessOrEualTwoNumbers2(): boolean {
-    let a = new u128(100, 100);
-    let b = new u128(100, 100);
-    let lt = b <= a;
-    return lt == true;
+    var a = new u128(100, 100);
+    var b = new u128(100, 100);
+    return b <= a;
   }
 
   static shouldCompareGreaterTwoNumbers1(): boolean {
-    let a = new u128(100, 100);
-    let b = new u128(50,  100);
-    let lt = a > b;
-    return lt == true;
+    var a = new u128(100, 100);
+    var b = new u128(50,  100);
+    return a > b;
   }
 
   static shouldCompareGreaterTwoNumbers2(): boolean {
-    let a = new u128(100, 100);
-    let b = new u128(100, 100);
-    let lt = a > b;
-    return lt == false;
+    var a = new u128(100, 100);
+    var b = new u128(100, 100);
+    return !(a > b);
   }
 
   static shouldCompareGreaterOrEqualTwoNumbers1(): boolean {
-    let a = new u128(100, 100);
-    let b = new u128(50,  100);
-    let lt = a >= b;
-    return lt == true;
+    var a = new u128(100, 100);
+    var b = new u128(50,  100);
+    return a >= b == true;
   }
 
   static shouldCompareGreaterOrEqualTwoNumbers2(): boolean {
-    let a = new u128(100, 100);
-    let b = new u128(100, 100);
-    let lt = a >= b;
-    return lt == true;
+    var a = new u128(100, 100);
+    var b = new u128(100, 100);
+    return a >= b == true;
   }
 
   static shouldAddTwoNumbers(): boolean {
-    let a = new u128(100, 255);
-    let b = new u128(255, 100);
-    let c = a + b;
-    return c == new u128(355, 355);
+    var a = new u128(100, 255);
+    var b = new u128(255, 100);
+    return a + b == new u128(355, 355);
   }
 
-  static shouldSubtractTwoNumbers(): boolean {
-    let c = new u128(355, 355);
-    let b = new u128(100, 255);
-    return c - b == new u128(255, 100);
+  static shouldSubtractTwoNumbers1(): boolean {
+    var a = new u128(355, 355);
+    var b = new u128(100, 255);
+    return a - b == new u128(255, 100);
+  }
+
+  static shouldSubtractFromZeroEqualNegativeNumber(): boolean {
+    var a = new u128(100, 255);
+    return u128.Zero - a == -a;
   }
 
   static shouldLeftShiftOneNumber1(): boolean {
-    let a = new u128(1, 0);
+    var a = new u128(1, 0);
     return a << 65 == new u128(0, 2);
   }
 
   static shouldPeriodicLeftShiftOneNumber(): boolean {
-    let a = new u128(1, 0);
+    var a = new u128(1, 0);
     return a << (65 + 128) == new u128(0, 2);
   }
 
   static shouldInvariantLeftShiftZeroNumber(): boolean {
-    let a = new u128(1, 1);
+    var a = new u128(1, 1);
     return a << 0 == a;
   }
 
   static shouldRightShiftOneNumber(): boolean {
-    let a = new u128(0, 100);
+    var a = new u128(0, 100);
     return a >> 65 == new u128(50, 0);
   }
 
   static shouldPeriodicRightShiftOneNumber(): boolean {
-    let a = new u128(0, 100);
+    var a = new u128(0, 100);
     return a >> (65 + 128) == new u128(50, 0);
   }
 
   static shouldInvariantRightShiftZeroNumber(): boolean {
-    let a = new u128(1, 1);
+    var a = new u128(1, 1);
     return a >> 0 == a;
   }
 
   static shouldMultiplyTwoNumbers(): boolean {
-    let a = u128.from(43545453452);
-    let b = u128.from(2353454354);
-    let c = a * b;
-    return c == new u128(10248516654965971928, 5);
+    var a = u128.from(43545453452);
+    var b = u128.from(2353454354);
+    return a * b == new u128(10248516654965971928, 5);
   }
 
   // static shouldDivideTwoNumbers(): boolean {
@@ -144,26 +142,26 @@ export class BasicOperationsTest {
   //   return c / a == b;
   // }
 
-  static shouldIncrementNumber1(): boolean {
-    let a = new u128(10248516654965971928, 5);
+  static shouldPrefixIncrementNumber1(): boolean {
+    var a = new u128(10248516654965971928, 5);
     ++a;
     return a == new u128(10248516654965971929, 5);
   }
 
-  static shouldIncrementNumber2(): boolean {
-    let a = new u128(0xFFFFFFFFFFFFFFFF, 0);
+  static shouldPrefixIncrementNumber2(): boolean {
+    var a = new u128(0xFFFFFFFFFFFFFFFF, 0);
     ++a;
     return a == new u128(0, 1);
   }
 
-  static shouldDecrementNumber1(): boolean {
-    let a = new u128(10248516654965971928, 5);
+  static shouldPrefixDecrementNumber1(): boolean {
+    var a = new u128(10248516654965971928, 5);
     --a;
     return a == new u128(10248516654965971927, 5);
   }
 
-  static shouldDecrementNumber2(): boolean {
-    let a = new u128(0, 1);
+  static shouldPrefixDecrementNumber2(): boolean {
+    var a = new u128(0, 1);
     --a;
     return a == new u128(0xFFFFFFFFFFFFFFFF, 0);
   }
