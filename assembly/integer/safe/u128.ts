9a00212cd544fe755ea76b2ed2d99832ea9c21e5
MaxGraey <maxgraey@gmail.com>
2018-08-05 19:42:07 +0300

    progress for safe.u128

--- assembly/integer/safe/u128.ts
+++ assembly/integer/safe/u128.ts
@@ -5,7 +5,9 @@ import { u128 as U128 } from '../u128';
 
     @inline @operator.prefix('++')
     preInc(): this {
-      if (this.lo == <u64>-1) assert(this.hi != <u64>-1);
+      if (this.lo == <u64>-1) {
+        assert(this.hi != <u64>-1, "overflow");
+      }
 
       // TODO
       // super.inc();
@@ -14,7 +16,9 @@ import { u128 as U128 } from '../u128';
 
     @inline @operator.prefix('--')
     preDec(): this {
-      if (!this.hi) assert(this.lo);
+      if (!this.hi) {
+        assert(this.lo, "overflow");
+      }
 
       // TODO
       // super.dec();
@@ -27,7 +31,7 @@ import { u128 as U128 } from '../u128';
       assert(b, "value shouldn't be null");
       // overflow guard
       // assert(clz(a.hi) | clz(b.hi));
-      assert(clz(a.hi | b.hi));
+      assert(clz(a.hi | b.hi), "overflow");
       return changetype<u128>(
         U128.add(changetype<U128>(a), changetype<U128>(b))
       );
@@ -38,7 +42,7 @@ import { u128 as U128 } from '../u128';
       assert(a, "value shouldn't be null");
       assert(b, "value shouldn't be null");
       // underflow guard
-      assert(changetype<U128>(a) >= changetype<U128>(b));
+      assert(changetype<U128>(a) >= changetype<U128>(b), "overflow");
       return changetype<u128>(
         U128.sub(changetype<U128>(a), changetype<U128>(b))
       );
@@ -54,7 +58,7 @@ import { u128 as U128 } from '../u128';
       var bzn = u128.clz(b);
 
       // overflow guard
-      assert(azn + bzn >= 128);
+      assert(azn + bzn >= 128, "overflow");
 
       // if a == 0 || b == 0
       if (!(azn - 128 & bzn - 128))
@@ -72,7 +76,10 @@ import { u128 as U128 } from '../u128';
     @inline @operator('**')
     static pow(base: u128, exponent: i32): u128 {
       assert(base, "value shouldn't be null");
-      assert(exponent <= 128);
+      assert(exponent <= 128, "overflow");
+      if (exponent > 1 && !U128.isZero(base)) {
+        assert(base.lo != 0, "overflow");
+      }
       // TODO assert overflow
 
       return changetype<u128>(U128.pow(changetype<U128>(base), exponent));
